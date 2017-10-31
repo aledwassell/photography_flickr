f6118273eb6cd911a8254ebdfbd067dab4f2332d
Aled's Pro! <pc-user@pc-user-no-MBP.home>
2017-10-31 22:32:44 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(){
+    'use strict';
+
+    angular.module('flickr_photo', ['ngRoute'])
+        .controller('representation_section_controller', ['$scope', function ($scope) {
+            $scope.me = 'Aled';
+
+        }]);
+
+
+})();
