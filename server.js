431369ec0259ceb3c496d6b49ee5947f82eceb52
Aled's Pro! <pc-user@pc-user-no-MBP.home>
2017-11-01 22:00:36 +0000

    test

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-//Load HTTP module
-var http = require("http");
+var express = require('express');
+var app = express;
 
-//Create HTTP server and listen on port 8000 for requests
-http.createServer(function (request, response) {
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
 
-    // Set the response HTTP header with HTTP status and Content type
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000!')
+});
 
-    // Send the response body "Hello World"
-    response.end('Hello World\n');
-}).listen(8000);
-
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/')
+//
+//
+// (function () {
+//     'use strict';
+//
+//
+//
+// })();
+//
