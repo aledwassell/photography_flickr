e32252b72f49f603f72d9d759fead716e5ba4605
Aled's Pro! <pc-user@pc-user-no-MBP.home>
2017-11-01 21:43:13 +0000

    test

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 (function(){
     'use strict';
 
-    angular.module('flickr_photo', [])
+    angular.module('flickr_photo', ['ngRoute'])
+        .config(function ($routeProvider) {
+            $routeProvider.when('/', {
+                controller: 'HomeController',
+                templateUrl: 'views/home.html'
+            })
+        })
         .controller('representation_section_controller', ['$scope', function ($scope) {
             $scope.me = 'Aled';
+            $scope.list = [
+                'Sheep',
+                'Goats',
+                'Bananas'
+            ]
         }]);
-
-
 })();
